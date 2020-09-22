4ecc882b5d9f49456d6d4922da1552455aee1b63
KalinkinaD <dkalinina8@gmail.com>
2020-09-22 16:55:31 +0300

    Updates

---
+++ precache-manifest.51de0ba0dda768bcaa6a5e32c8193388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadbb0cb6655533afdcb6c0ce53529c8",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "898b0f9ac86c194adb3f",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.24f336f2.chunk.css"
+  },
+  {
+    "revision": "614a15296e1590f8b27e",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.0cf05676.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.0cf05676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898b0f9ac86c194adb3f",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.60065c70.chunk.js"
+  },
+  {
+    "revision": "e3f60b8770ae85b5a663",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.6ec4936c.js"
+  }
+]);
